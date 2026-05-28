27440866199cbd6bf8d9823c4e6fd6e6944b6657
Tarunswamy-Muralidharan <tmswamy10@gmail.com>
2026-05-28 05:32:27 +0000

    Auto-sync [2026-05-28 05:32]: 4 modified

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.justpass.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "3.0.3"
+        versionCode = 14
+        versionName = "3.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
